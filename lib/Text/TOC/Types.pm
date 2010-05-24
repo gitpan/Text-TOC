0378d8557ff9aedeb6575975489a8fd2e05aeb17
Dave Rolsky <autarch@urth.org>
2010-05-24 18:40:08 +0000

    Import of DROLSKY/Text-TOC-0.01 from CPAN.
    gitpan-cpan-distribution: Text-TOC
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DROLSKY/Text-TOC-0.01.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

---
+++ lib/Text/TOC/Types.pm
@@ -0,0 +1,51 @@
+package Text::TOC::Types;
+BEGIN {
+  $Text::TOC::Types::VERSION = '0.01';
+}
+
+use strict;
+use warnings;
+
+use base 'MooseX::Types::Combine';
+
+__PACKAGE__->provide_types_from(
+    qw( Text::TOC::Types::Internal
+        MooseX::Types::Moose
+        MooseX::Types::Path::Class
+        )
+);
+
+1;
+
+# ABSTRACT: Provides types for use in Text::TOC
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Text::TOC::Types - Provides types for use in Text::TOC
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This class exports the types from L<Text::TOC::Types::Internal>,
+L<MooseX::Types::Moose> , and L<MooseX::Types::Path::Class>.
+
+=head1 AUTHOR
+
+  Dave Rolsky <autarch@urth.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Dave Rolsky.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Text/TOC/Types/Internal.pm
@@ -0,0 +1,61 @@
+package Text::TOC::Types::Internal;
+BEGIN {
+  $Text::TOC::Types::Internal::VERSION = '0.01';
+}
+
+use strict;
+use warnings;
+use namespace::autoclean;
+
+use MooseX::Types -declare => [
+    qw(
+        Filter
+        Node
+        )
+];
+
+use MooseX::Types::Moose qw( CodeRef );
+
+role_type Filter, { role => 'Text::TOC::Role::Filter' };
+
+coerce Filter,
+    from CodeRef,
+    via { Text::TOC::Filter::Anon->new( code => $_ ) };
+
+role_type Node, { role => 'Text::TOC::Role::Node' };
+
+require Text::TOC::Filter::Anon;
+
+1;
+
+# ABSTRACT: Defines types specific to Text::TOC
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Text::TOC::Types::Internal - Defines types specific to Text::TOC
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This class defines several types used internally in Text::TOC.
+
+=head1 AUTHOR
+
+  Dave Rolsky <autarch@urth.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Dave Rolsky.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
